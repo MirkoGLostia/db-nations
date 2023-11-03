9e36e93a0f5dd8ea47062351b215449e6e714de7
MirkoGLostia <giggi.torre@mail.com>
2023-11-03 15:02:57 +0100

    user research of the country

--- src/org/exercise/java/nations/MainNation.java
+++ src/org/exercise/java/nations/MainNation.java
@@ -1,29 +1,36 @@
 package org.exercise.java.nations;
 
 import java.sql.*;
+import java.util.Scanner;
 
 public class MainNation {
     public static void main(String[] args) {
+
+        Scanner userInput = new Scanner(System.in);
 
         // parameters for the connection to the database
         String url = "jdbc:mysql://localhost:3306/db_nations";
         String user = "root";
         String password = "root";
 
+
         // try to open a connection
         try (Connection connection = DriverManager.getConnection(url, user, password)) {
+            System.out.println("what country you want to search?");
+            String userCountry = userInput.nextLine();
 
             String query =
                     "select c.country_id, c.name, r.name, c2.name "
                             + "from countries c "
                             + "join regions r on r.region_id = c.region_id "
                             + "join continents c2 on c2.continent_id = r.continent_id "
-                            + "order by c.name ";
+                            + "where lower(c.name) like ? "
+                            + "order by c.name; ";
 
             try (PreparedStatement preparedStatement = connection.prepareStatement(query)) {
 
                 // insert scanner variables into the query
-
+                preparedStatement.setString(1, "%" + userCountry + "%");
 
 
                 try (ResultSet resultSet = preparedStatement.executeQuery()) {
@@ -52,5 +59,8 @@ public class MainNation {
             System.out.println("Unable to open connection");
             e.printStackTrace();
         }
+
+
+        userInput.close();
     }
 }
