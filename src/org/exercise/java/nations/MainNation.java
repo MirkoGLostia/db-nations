312a5814e6b1f9651619b302095e813c8ba0448e
MirkoGLostia <giggi.torre@mail.com>
2023-11-03 14:42:33 +0100

    write the data in the terminal

--- src/org/exercise/java/nations/MainNation.java
+++ src/org/exercise/java/nations/MainNation.java
@@ -1,8 +1,6 @@
 package org.exercise.java.nations;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
+import java.sql.*;
 
 public class MainNation {
     public static void main(String[] args) {
@@ -16,11 +14,38 @@ public class MainNation {
         try (Connection connection = DriverManager.getConnection(url, user, password)) {
 
             String query =
-                    "select c.name, c.country_id, r.name, c2.name "
+                    "select c.country_id, c.name, r.name, c2.name "
                             + "from countries c "
                             + "join regions r on r.region_id = c.region_id "
                             + "join continents c2 on c2.continent_id = r.continent_id "
                             + "order by c.name ";
+
+            try (PreparedStatement preparedStatement = connection.prepareStatement(query)) {
+
+                // insert scanner variables into the query
+
+
+
+                try (ResultSet resultSet = preparedStatement.executeQuery()) {
+
+                    while (resultSet.next()) {
+                        String countryID = resultSet.getString("c.country_id");
+                        String countryName = resultSet.getString("c.name");
+                        String regionName = resultSet.getString("r.name");
+                        String continentName = resultSet.getString("c2.name");
+
+                        System.out.println(countryID + " - " + countryName + " REGION: " + regionName + " CONTINENT: " + continentName);
+                    }
+
+                } catch (SQLException e) {
+                    System.out.println("Unable to execute query");
+                    e.printStackTrace();
+                }
+
+            } catch (SQLException e) {
+                System.out.println("Unable to prepare statement");
+                e.printStackTrace();
+            }
 
 
         } catch (SQLException e) {
